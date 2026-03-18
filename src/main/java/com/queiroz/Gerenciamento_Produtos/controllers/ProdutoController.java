e3f1df67ef423defd831024ee747303461d3017f
queiroz <ferqueiroz2205@gmail.com>
2026-03-17 21:49:14 -0300

    Finalizando a parte de requisições.

--- src/main/java/com/queiroz/Gerenciamento_Produtos/controllers/ProdutoController.java
+++ src/main/java/com/queiroz/Gerenciamento_Produtos/controllers/ProdutoController.java
@@ -1,13 +1,44 @@
 package com.queiroz.Gerenciamento_Produtos.controllers;
 
+import com.queiroz.Gerenciamento_Produtos.models.ProdutoModel;
 import com.queiroz.Gerenciamento_Produtos.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<ProdutoModel> criarProduto(@RequestBody ProdutoModel produtoModel) {
+        return ResponseEntity.status(201).body(produtoService.criarProduto(produtoModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModel>> listarProdutos() {
+        return ResponseEntity.ok().body(produtoService.listarProdutos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoModel> buscarProduto(@PathVariable Long id) {
+        Optional<ProdutoModel> livro = produtoService.buscarProduto(id);
+
+        if (livro.isPresent())
+            return ResponseEntity.ok().body(livro.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProdutoModel> deletarProduto(@PathVariable Long id) {
+        produtoService.excluirProduto(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/queiroz/Gerenciamento_Produtos/services/ProdutoService.java
+++ src/main/java/com/queiroz/Gerenciamento_Produtos/services/ProdutoService.java
@@ -1,11 +1,33 @@
 package com.queiroz.Gerenciamento_Produtos.services;
 
+import com.queiroz.Gerenciamento_Produtos.models.ProdutoModel;
 import com.queiroz.Gerenciamento_Produtos.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    public ProdutoModel criarProduto(ProdutoModel produtoModel) {
+        return produtoRepository.save(produtoModel);
+    }
+
+    public List<ProdutoModel> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<ProdutoModel> buscarProduto(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public void excluirProduto(Long id) {
+        produtoRepository.deleteById(id);
+    }
 }
